19b05c1160508cac29cec961697dc2d5fe07157e
FachryRizano <fachry.rizano@gmail.com>
2020-11-02 18:09:42 +0700

    memodifikasi repository layer

--- backend-springboot-app/src/main/java/com/example/demo/repository/UserRepository.java
+++ backend-springboot-app/src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.User;
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
