cecf249df8d6085f64aed086537ba999f07a571d
FachryRizano <fachry.rizano@gmail.com>
2020-11-03 11:10:30 +0700

    menghapus package - package

--- backend-springboot-app/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User findByEmail(String email);
-    User save(UserRegistrationDto registration);
-}
