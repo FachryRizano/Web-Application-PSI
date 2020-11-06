765c3e8642a362cc901d47529a6175b14380d629
FachryRizano <fachry.rizano@gmail.com>
2020-11-06 12:58:02 +0700

    mengganti LoginEventRepository dengan LogEventRepository

--- backend-springboot-app/src/main/java/com/example/demo/repository/LogEventRepository.java
+++ backend-springboot-app/src/main/java/com/example/demo/repository/LogEventRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.LogEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoginEventRepository extends JpaRepository<LoginEvent,Long> {
+public interface LogEventRepository extends JpaRepository<LogEvent, Long> {
 
 }
