63668b4cd1982ac5ee228ef73bdbe7f5350d9ac6
FachryRizano <fachry.rizano@gmail.com>
2020-11-06 11:43:11 +0700

    membuat LoginEventService

--- backend-springboot-app/src/main/java/com/example/demo/repository/LoginEventRepository.java
+++ backend-springboot-app/src/main/java/com/example/demo/repository/LoginEventRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginEventRepository extends JpaRepository<LoginEvent,Long> {
-    
+
 }
---
+++ backend-springboot-app/src/main/java/com/example/demo/service/LoginEventService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.model.LoginEvent;
+import com.example.demo.repository.LoginEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class LoginEventService {
+    @Autowired
+    private LoginEventRepository loginEventRepository;
+
+    public void save(LoginEvent loginEvent){
+        loginEventRepository.save(loginEvent);
+    }
+}
