4cc6d2c17079093b5cd78ca4dee11e08898b84ce
FachryRizano <fachry.rizano@gmail.com>
2020-11-06 11:37:30 +0700

    membuat LoginEventRepository

---
+++ backend-springboot-app/src/main/java/com/example/demo/repository/LoginEventRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.LoginEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginEventRepository extends JpaRepository<LoginEvent,Long> {
+    
+}
