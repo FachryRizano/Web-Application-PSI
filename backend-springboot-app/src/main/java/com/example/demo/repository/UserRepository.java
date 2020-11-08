79829a49d223f3c4e0950a6482c21ab31e4cbc82
FachryRizano <fachry.rizano@gmail.com>
2020-11-08 19:02:14 +0700

    memodifikasi UserRepo'

--- backend-springboot-app/src/main/java/com/example/demo/repository/UserRepository.java
+++ backend-springboot-app/src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,5 +8,9 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
