a6ebcd7c35e7fb5b9a531e96b5c68b0ca50aac0c
FachryRizano <fachry.rizano@gmail.com>
2020-11-08 09:05:53 +0700

    implement AuthenticationEntryPoint ke RestAuthEntryPoint

--- backend-springboot-app/src/main/java/com/example/demo/config/RestAuthEntryPoint.java
+++ backend-springboot-app/src/main/java/com/example/demo/config/RestAuthEntryPoint.java
@@ -1,4 +1,18 @@
 package com.example.demo.config;
 
-public class RestAuthEntryPoint {
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RestAuthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        //send error response to the client (401 unauthorized)
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+    }
 }
