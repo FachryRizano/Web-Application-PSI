b20c0e0fd9394dcd18462000cb9e18f69b280592
FachryRizano <fachry.rizano@gmail.com>
2020-11-16 14:53:45 +0700

    membuat Read all dan create REST API untuk training schedule

--- backend-springboot-app/src/main/java/com/example/demo/controller/TrainingScheduleController.java
+++ backend-springboot-app/src/main/java/com/example/demo/controller/TrainingScheduleController.java
@@ -4,16 +4,28 @@ import com.example.demo.model.TrainingSchedule;
 import com.example.demo.repository.TrainingScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RequestMapping("api/test/v1/")
 @RestController
 public class TrainingScheduleController {
     @Autowired
     private TrainingScheduleRepository trainingScheduleRepository;
-    
+
+    //get all training schedule
+    @GetMapping("training-schedules")
     public List<TrainingSchedule> getAllEmployee(){
-        return
+        return trainingScheduleRepository.findAll();
     }
+
+    //create a training schedule
+    @PostMapping("training-schedules")
+    public TrainingSchedule createTrainingSchedule(@RequestBody TrainingSchedule trainingSchedule){
+        return trainingScheduleRepository.save(trainingSchedule);
+    }
+
+    
+
+
 }
