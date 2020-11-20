5ef55b7050b8a692a91a28fe25bd239d6136fb70
FachryRizano <fachry.rizano@gmail.com>
2020-11-20 13:24:21 +0700

    membuat Training Request Entitiy

--- backend-springboot-app/src/main/java/com/example/demo/model/trainingRequest/TrainingRequest.java
+++ backend-springboot-app/src/main/java/com/example/demo/model/trainingRequest/TrainingRequest.java
@@ -1,9 +1,25 @@
 package com.example.demo.model.trainingRequest;
 
+import java.time.Year;
+
 public class TrainingRequest {
-    private String nama;
-    private int golongan;
-    private String posisi;
-    private String keluargaJabatan;
-    private String level;
+    private Long id;
+    //Userdetails sepertinya entity sendiri
+    /*
+    - Nama
+    - Golongan
+    - NIK
+    - Posisi
+    - Keluarga Jabatan
+    - Level
+    - No HP
+    - Ukuran Kaos
+    - Entitas
+    - Divisi
+    - Kota
+    - PIC HC
+    * */
+    //Periode
+    private Year periode;
+
 }
