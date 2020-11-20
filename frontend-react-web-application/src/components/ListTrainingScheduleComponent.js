5feca19212b6f0948905ecd22cacf7f99fe99a20
FachryRizano <fachry.rizano@gmail.com>
2020-11-20 10:42:52 +0700

    menghilangkan bug ViewTrainingSchedule component dengan benar

--- frontend-react-web-application/src/components/ListTrainingScheduleComponent.js
+++ frontend-react-web-application/src/components/ListTrainingScheduleComponent.js
@@ -7,68 +7,69 @@ class ListTrainingScheduleComponent extends Component {
         super(props);
         this.state={
             trainingSchedules:[],
+            getCurrentUser:AuthService.getCurrentUser()
         }
     }
 
     componentDidMount(){
+        if(this.state.getCurrentUser == null){
+            return 
+        }else{
         TrainingScheduleService.getAllTrainingSchedule().then(
             res=>{
                 this.setState({
                     trainingSchedules : res.data      
                 });
-            },
-            error=>{
-                this.setState({
-
-                })
             }
             )
+        }
     }
     render() {
-        if(!AuthService.getCurrentUser()){
+        if(this.state.getCurrentUser==null){
             return <Redirect to="/login"/>
-        }
-        return (
-            <div>
-                 <h2 className="text-center">Training Schedule List</h2>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <td>Training Schedule Id</td>
-                                <td>Training Schedule Code</td>
-                                <td>Training Duration</td>
-                                <td>Training Subject Name</td>
-                                <td>Participants</td>
-                                <td>Schedules</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.trainingSchedules.map(
-                                    ts=>
-                                    <tr key = {ts.id}>
-                                        <td>{ts.id}</td>
-                                        <td>{ts.code}</td>
-                                        <td>{ts.duration}</td>
-                                        <td>{ts.subjectName}</td>
-                                        <td>{ts.participant.map(p=>p.name +"\n")}</td>
-                                        <td>{ts.schedules.map(s=>
-                                            "Schedules \n" + 
-                                            s.date + "\n" + 
-                                            s.linkPDF + "\n" + 
-                                            s.location + "\n" +
-                                            s.price + "\n" +
-                                            s.speakerName
-                                        )}</td>
-                                    </tr>
-                                )   
-                            }
-                        </tbody>
-                    </table>
+        }else{
+            return (
+                <div>
+                    <h2 className="text-center">Training Schedule List</h2>
+                    <div className="row">
+                        <table className="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <td>Training Schedule Id</td>
+                                    <td>Training Schedule Code</td>
+                                    <td>Training Duration</td>
+                                    <td>Training Subject Name</td>
+                                    <td>Participants</td>
+                                    <td>Schedules</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.trainingSchedules.map(
+                                        ts=>
+                                        <tr key = {ts.id}>
+                                            <td>{ts.id}</td>
+                                            <td>{ts.code}</td>
+                                            <td>{ts.duration}</td>
+                                            <td>{ts.subjectName}</td>
+                                            <td>{ts.participant.map(p=>p.name +"\n")}</td>
+                                            <td>{ts.schedules.map(s=>
+                                                "Schedules \n" + 
+                                                s.date + "\n" + 
+                                                s.linkPDF + "\n" + 
+                                                s.location + "\n" +
+                                                s.price + "\n" +
+                                                s.speakerName
+                                            )}</td>
+                                        </tr>
+                                    )   
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
 
     }
 }
