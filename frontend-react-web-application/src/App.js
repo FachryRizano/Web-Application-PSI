81d7a3fc67eeefdcb5fa3748ccdfd8ac37c68806
FachryRizano <fachry.rizano@gmail.com>
2020-11-25 11:38:51 +0700

    modified:   frontend-react-web-application/src/App.js

--- frontend-react-web-application/src/App.js
+++ frontend-react-web-application/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import {Route, Link, Switch, Redirect}from 'react-router-dom';
+import {Route, Link, Switch}from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./css/main.css";
