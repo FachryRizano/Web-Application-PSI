9e787b797770510fe4f710c41b10fce4cf4bcc9f
Feldy Tanudjaja <feldytanudjaja@Feldys-MacBook-Air.local>
2020-11-30 13:05:18 +0700

    modified by feldy

--- frontend-react-web-application/src/components/TrainingSchedule/TrainingRequestComponent.jsx
+++ frontend-react-web-application/src/components/TrainingSchedule/TrainingRequestComponent.jsx
@@ -9,7 +9,7 @@ class TrainingRequestComponent extends Component {
         super(props)
         this.state={
             user:authService.getCurrentUser()
-            
+
         }
     }
 
@@ -34,10 +34,10 @@ class TrainingRequestComponent extends Component {
             <Fragment>
                 <form>
                     <div className="container bg-light">
-                        
+
                         <div className="row">
                             <label>Name:<input type="text" name="name" /></label>
-                            
+
                             <div className="col-md-6">
                                 <label>Golongan:<input type="text" name="golongan" /></label>
                                 <label>NIK:<input type="text" name="NIK" /></label>
@@ -47,26 +47,26 @@ class TrainingRequestComponent extends Component {
                                 <label>No.HP:<input type="text" name="noHP" /></label>
                                 <label>Ukuran Kaos:<input type="text" name="kaos" /></label>
                             </div>
-                            
+
                             <div className="col-md-6">
                                 <label>Entitas:<input type="text" name="entitas" /></label>
                                 <label>Divisi:<input type="text" name="divisi" /></label>
                                 <label>Kota:<input type="text" name="kota" /></label>
-                                <label>PIC HC:<input type="text" name="picHC" /></label>    
+                                <label>PIC HC:<input type="text" name="picHC" /></label>
                             </div>
                         </div>
 
                         <div className="row">
-                            <label>Periode:<input type="text" name="periode" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
-                            <label>:<input type="text" name="" /></label>    
+                            <label>Periode:<input type="text" name="periode" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
+                            <label>:<input type="text" name="" /></label>
                         </div>
 
 
